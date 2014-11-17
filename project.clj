0bf9134c8348c7b035909ddfedfc549efd73ffa1
fogus <mefogus@gmail.com>
2014-11-17 12:13:58 -0500

    Project file

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject phenomena "0.1.0-SNAPSHOT"
+  :description "TODO"
+  :url "http://www.github.com/fogus/phenomena"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]
+  :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]])
