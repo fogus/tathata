3b3c17228469a589b38f8594ea780e3383518f79
fogus <mefogus@gmail.com>
2014-11-17 14:30:20 -0500

    naive cut of thread-cells

--- src/clj/phenomena/impl/thread_cell.clj
+++ src/clj/phenomena/impl/thread_cell.clj
@@ -1,3 +1,32 @@
 (ns phenomenon.impl.thread-cell
   (:require phenomenon.core))
 
+(deftype ThreadCell [thread
+                     axioms
+                     ^:unsynchronized-mutable val
+                     ^:unsynchronized-mutable trans]
+  Object
+  (equals [this o] (identical? this o))
+  (hashCode [this] (System/identityHashCode this))
+  (toString [_] "") ;; TODO 
+
+  clojure.lang.IMeta
+  (meta [_] {}) ;; TODO
+
+  clojure.lang.IDeref
+  (deref [this] (cell-render this))
+
+  Cell
+  (cell-sentry [_] thread)
+  (cell-get-transient [_]
+    ;; TODO: check precepts
+    (when (identical? ::none trans)
+      (set! trans (core/transient-of val)))
+    trans)
+  (cell-set-transient [this t] (set! trans t) this)
+  (cell-render [_]
+    ;; TODO: check precepts
+    (when-not (identical? trans ::none)
+      (set! val (core/value-of trans))
+      (set! trans ::none))
+    val))
