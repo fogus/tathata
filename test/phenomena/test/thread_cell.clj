18bae68b1c40933afb556d13db2613475efa4ba4
fogus <mefogus@gmail.com>
2014-11-20 11:05:19 -0500

    testing transients, second transient test

--- test/phenomena/test/thread_cell.clj
+++ test/phenomena/test/thread_cell.clj
@@ -49,5 +49,11 @@
     (is (= @v1 [0 1 2 3 4 5 6 7 8 9]))
     (is (= (count @v1) 10))
 
-))
+    (dotimes [i 10]
+      (phenomena.core/pass
+       conj!
+       v2
+       (phenomena.core/fetch count v2)))
+
+    (is (= @v1 [0 1 2 3 4 5 6 7 8 9]))))
 
