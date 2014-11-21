61db304658324e05b696467669eccdf8419f2f62
fogus <mefogus@gmail.com>
2014-11-21 11:03:11 -0500

    MOving protos into their own ns

---
+++ src/clj/phenomena/protocols.clj
@@ -0,0 +1,18 @@
+(ns phenomena.protocols)
+
+(defprotocol Editable
+  (transient-of [value] [value this]))
+
+(defprotocol Transient
+  (value-of [transient] [transient this]))
+
+(defprotocol Axiomatic
+  (precept [this]))
+
+(defprotocol Sentry
+  (make-cell [sentry val]))
+
+(defprotocol Cell
+  (cell-get-transient [cell])
+  (cell-set-transient [cell t])
+  (cell-render [cell]))
