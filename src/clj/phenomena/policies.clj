6363024709da0f3fe4e629bf744dcb1ad98f22a1
fogus <mefogus@gmail.com>
2014-11-21 12:04:34 -0500

    Adding a construct only policy.

--- src/clj/phenomena/policies.clj
+++ src/clj/phenomena/policies.clj
@@ -10,3 +10,11 @@
   (precept [_] (identical? (Thread/currentThread) thread))
   (precept-failure-msg [_] "You cannot access this pod across disparate threads."))
 
+
+(defrecord ConstructOnly [thread]
+  phenomena.protocols/Sentry
+  (make-cell [this val] (tc/->ThreadCell this val :phenomena.core/nothing))
+
+  phenomena.protocols/Axiomatic
+  (precept [_] false)
+  (precept-failure-msg [_] "You cannot access this pod after construction."))
