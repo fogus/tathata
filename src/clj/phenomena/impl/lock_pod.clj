a991c54b0e710189892568a7ec372e386b1442d7
fogus <mefogus@gmail.com>
2014-12-02 15:49:44 -0500

    Sentry logic in comment block.

--- src/clj/phenomena/impl/lock_pod.clj
+++ src/clj/phenomena/impl/lock_pod.clj
@@ -1,10 +1,11 @@
 (ns phenomena.impl.lock-pod
-  (:require phenomena.protocols))
+  (:require phenomena.protocols
+            phenomena.core))
 
-(deftype LockCell [lock
-                   ^:volatile-mutable val
-                   ^:unsynchronized-mutable trans
-                   _meta]
+(deftype LockPod [lock
+                  ^:volatile-mutable val
+                  ^:unsynchronized-mutable trans
+                  _meta]
   Object
   (equals [this o] (identical? this o))
   (hashCode [this] (System/identityHashCode this))
@@ -28,15 +29,15 @@
   phenomena.protocols/Pod
   (pod-get-transient [_]
     (assert (.isHeldByCurrentThread lock))
-    (when (identical? ::none trans)
+    (when (identical? :phenomena.core/nothing trans)
       (set! trans (phenomena.protocols/transient-of val)))
     trans)
   (pod-set-transient [this t] (set! trans t) this)
   (pod-render [_]
     (assert (.isHeldByCurrentThread lock))
-    (when-not (identical? trans ::none)
+    (when-not (identical? trans :phenomena.core/nothing)
       (set! val (phenomena.protocols/value-of trans))
-      (set! trans ::none))
+      (set! trans :phenomena.core/nothing))
     val)
 
   Object
@@ -52,6 +53,24 @@
     phenomena.protocols/Transient
     (value-of [sb] (.toString sb)))
 
+  (def lp (LockPod. (java.util.concurrent.locks.ReentrantLock. true) ;; fair?
+                    ""
+                    :phenomena.core/nothing
+                    {}))
+
+  @lp ;;=> ""
+
+  (def ^:dynamic *in-cells* nil)
   
+  (let [lock (.lock lp)]
+    (assert lock)
+    (assert (nil? *in-cells*))
+    (binding [*in-cells* true]
+      (.lock lock)
+      (try
+        (dotimes [i 10]
+          (phenomena.core/pass .append #^StringBuilder lp i))
+        @lp
+        (finally (.unlock lock)))))
 
 )
