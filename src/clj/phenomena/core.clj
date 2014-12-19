bfe3bfa39e46056397a4561d3d293ada9b2eab41
fogus <mefogus@gmail.com>
2014-12-19 11:36:39 -0500

    Target was never used.

--- src/clj/phenomena/core.clj
+++ src/clj/phenomena/core.clj
@@ -2,10 +2,6 @@
   (:require phenomena.protocols
             phenomena.policies))
 
-(defmacro target [f pod & args]
-  `(let [f# (fn [tgt#] (~f tgt# ~@args) tgt#)]
-     (pod-set-transient ~pod (f# ~(with-meta `(phenomena.protocols/pod-get-transient ~pod) (meta pod))))))
-
 (defmacro pass [f pod & args]
   `(phenomena.protocols/pod-set-transient ~pod (~f ~(with-meta `(phenomena.protocols/pod-get-transient ~pod) (meta pod)) ~@args)))
 
