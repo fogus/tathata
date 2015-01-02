24ce9ee2e8f3019b96e8343f2900df3c3931f156
fogus <mefogus@gmail.com>
2015-01-02 13:31:39 -0500

    first cut of guarded macro

--- src/clj/phenomena/core.clj
+++ src/clj/phenomena/core.clj
@@ -12,3 +12,16 @@
   ([val] (pod val (phenomena.policies/->SingleThreadedRWAccess (Thread/currentThread))))
   ([val policy] (phenomena.protocols/make-pod policy val)))
 
+(defmacro guarded [pods & body]
+  (case (count pods)
+    0 `(do ~@body)
+    1 `(phenomena.protocols/coordinated (:policy ~@pods) (fn [] ~@body))
+    `(phenomena.protocols/coordinated   (:policy ~(first pods)) (fn ~pods ~@body) [~@pods]))) ;; TODO: first pods too loose?
+
+(comment
+
+  (macroexpand '(guarded [c] (dotimes [i 10] @c)))
+  (macroexpand '(guarded [c d] (dotimes [i 10] [@c @d])))
+
+)
+
