2744de3733fefed73cc8a2ce0262b19d5977b24b
fogus <mefogus@gmail.com>
2015-01-05 22:31:15 -0500

    commentary

--- src/clj/phenomena/core.clj
+++ src/clj/phenomena/core.clj
@@ -9,15 +9,23 @@
   (:require [phenomena.protocols :as proto]))
 
 (defmacro via
-  ""
-  [f pod & args]
+  "Calls a method or function `op` through the given `pod` with
+   the supplied arguments.  This macro is intended to be used
+   for operations that might mutate the transient held  in the
+   pod and guarded / coordianted by the pod's policy."
+  [op pod & args]
   `(proto/pod-set-transient
     ~pod
-    (~f ~(with-meta `(proto/pod-get-transient ~pod) (meta pod))
+    (~op ~(with-meta `(proto/pod-get-transient ~pod) (meta pod))
         ~@args)))
 
-(defmacro fetch [f pod & args]
-  `(~f ~(with-meta `(proto/pod-get-transient ~pod) (meta pod))
+(defmacro fetch
+  "Calls a method or function `op` through the given `pod` wiht
+   the supplied arguments.  This macro is intended to be used
+   for operations that read the value of the transient held in the
+   pod and guarded / coordinated by the pod's policy."
+  [op pod & args]
+  `(~op ~(with-meta `(proto/pod-get-transient ~pod) (meta pod))
        ~@args))
 
 (defn pod [val policy]
