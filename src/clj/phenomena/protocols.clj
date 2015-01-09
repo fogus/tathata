88150ab21943029fcb59a1c614cb550b02564aa0
fogus <mefogus@gmail.com>
2015-01-08 22:57:22 -0500

    documentation of protocols.

--- src/clj/phenomena/protocols.clj
+++ src/clj/phenomena/protocols.clj
@@ -5,9 +5,20 @@
 ;   By using this software in any fashion, you are agreeing to be bound by
 ;   the terms of this license.
 ;   You must not remove this notice, or any other, from this software.
-(ns phenomena.protocols)
+(ns phenomena.protocols
+  "This namespace contains only the protocols used to create and
+   extend pods.  All pod and policy operations will occur in
+   relation to these protocols, including:
+
+   - Turning a value into a transient reprsentation.
+   - Turning a transient into a value.
+   - Performing checks to ensure that pod operations can be executed.
+   - Sentry operations for pod creation and comparison.
+   - Coordination.
+   - Pod mutation, fetch, and rendering operations.")
 
 (defprotocol Editable
+  ""
   (transient-of [value] [value this]))
 
 (defprotocol Transient
