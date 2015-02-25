d14ee837dd917e66eca781ba7b1fd4d5f4a24fd7
fogus <mefogus@gmail.com>
2015-02-25 15:29:08 -0500

    Changing the naming slightly to be more precise

--- src/clj/phenomena/impl/general_pod.clj
+++ src/clj/phenomena/impl/general_pod.clj
@@ -15,8 +15,8 @@
 ;;
 ;; The `^:unsynchronized-mutable` is, for lack of a better term
 ;; a pattern for creating pods.  That is, since pods are meant to
-;; hold a transient object there should be some modecum of protection.
-;; That modecum is of course just to make the transient and its
+;; hold an ephemeral object there should be some modecum of protection.
+;; That modecum is of course just to make the object and its
 ;; representational value private properties of the pod itself. Now,
 ;; how these "assignables" are set is entirely up to the disgression
 ;; of the pod creator.
@@ -31,12 +31,12 @@
 ;;
 (deftype GeneralPod [policy ;; every pod has a policy   
                      ^:unsynchronized-mutable val ;; This gens a Java private variable
-                     ^:unsynchronized-mutable trans
+                     ^:unsynchronized-mutable noumenon
                      _meta]
   Object
   (equals [this o] (identical? this o))
   (hashCode [this] (System/identityHashCode this))
-  (toString [_] (str "#<GeneralPod:" policy " ["(if val val trans) "]>"))
+  (toString [_] (str "#<GeneralPod:" policy " ["(if val val noumenon) "]>"))
 
   Comparable
   (compareTo [this o]
@@ -52,34 +52,36 @@
   ;; A pod has only three operations: get, set, and render.
   ;; 
   phenomena.protocols/Pod
-  ;; A pod is a transient object that holds a transient object.
+  ;; A pod is a shell type that holds a mutable object.  While the
+  ;; name `noumenon` is used in this context, a "getter" is
+  ;; immediately presented to retrieve it...
   ;; ... YOLO
   ;;
   (pod-get-transient [this]
     (phenomena.protocols/precept-get policy this)
-    (when (identical? :phenomena.core/nothing trans)
-      (set! trans (phenomena.protocols/value->transient val)))
-    trans)
+    (when (identical? :phenomena.core/nothing noumenon)
+      (set! noumenon (phenomena.protocols/value->transient val)))
+    noumenon)
   
   (pod-set-transient [this t]
     (phenomena.protocols/precept-set policy this)
-    (set! trans t) this)
+    (set! noumenon t) this)
 
   ;; Of the three pod operations, `render` is the most interesting.
-  ;; That is, pod rendering is the process of turning a transient
+  ;; That is, pod rendering is the process of turning a mutable
   ;; object into a value.  However, rendering is also a destructive
-  ;; process that wipes the transient object in favor of the
+  ;; process that wipes the noumenal object in favor of the
   ;; value itself.  It's expected that any mutations of the underlying
   ;; object will trigger a `pod-set-transient` operations, but of course
-  ;; there's no way to really enforce that, *except* that the `via` macro
-  ;; enforces that the condition holds. However, if you don't go through
-  ;; the `via` macro then there's no guarantee that the transient object
-  ;; will be what you expect it to be when you expect it to be.  YOLO
+  ;; there's no provided way to really enforce that, *except* that the `via` 
+  ;; macro enforces that the condition holds. However, if you don't go through
+  ;; the `via` macro then there's no guarantee that the noumenal object
+  ;; will be what you expect it to be when you expect it to be.
   (pod-render [this]
     (phenomena.protocols/precept-render policy this)
-    (when-not (identical? trans :phenomena.core/nothing)
-      (set! val (phenomena.protocols/transient->value trans))
-      (set! trans :phenomena.core/nothing))
+    (when-not (identical? noumenon :phenomena.core/nothing)
+      (set! val (phenomena.protocols/transient->value noumenon))
+      (set! noumenon :phenomena.core/nothing))
     val))
 
 
