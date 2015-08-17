173dfc31e0821495cc3bb27a46a817d1964bfeab
fogus <mefogus@gmail.com>
2015-08-16 23:18:39 -0400

    MOving to 1.8.0 SNAP

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://www.github.com/fogus/phenomena"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]
   :plugins [[lein-marginalia "0.8.0"]
             [org.clojure/test.check "0.7.0"]]
   :source-paths ["src/clj"]
