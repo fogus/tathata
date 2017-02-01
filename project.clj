11a1f0e99b3627b92c1efd7dc46e3966cb6d8b2f
fogus <mefogus@gmail.com>
2017-02-01 10:22:22 -0500

    Added Alpha 12 dep

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://www.github.com/fogus/tathata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha5"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
   :plugins [[lein-marginalia "0.8.0"]
-            [org.clojure/test.check "0.7.0"]]
+            [org.clojure/test.check "0.9.0"]]
   :source-paths ["src/clj"]
   :test-paths ["test"]
   :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]])
