22aa71c732f8616c64375657eb1e744a342b9557
fogus <mefogus@gmail.com>
2017-04-06 10:37:46 -0400

    Bumping the clj version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://www.github.com/fogus/tathata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]]
   :plugins [[lein-marginalia "0.8.0"]
             [org.clojure/test.check "0.9.0"]]
   :source-paths ["src/clj"]
