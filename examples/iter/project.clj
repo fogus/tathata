2472d62fc907ae707a38c480c78619de5a927cde
Fogus <mefogus@gmail.com>
2020-04-28 09:41:54 -0400

    bump clj version

--- examples/iter/project.clj
+++ examples/iter/project.clj
@@ -3,7 +3,7 @@
   :url "http://www.github.com/fogus/tathata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [fogus/tathata "0.2.5-SNAPSHOT"]]
   :plugins [[lein-marginalia "0.8.0"]
             [org.clojure/test.check "0.7.0"]]  
